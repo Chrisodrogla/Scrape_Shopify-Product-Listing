30c2445c2ede88e77bc25f210acf5e4336f539e1
Christian Algordo <123637289+Chrisodrogla@users.noreply.github.com>
2024-02-08 06:34:50 +0800

    Update dannerlac_data_editor.py

--- dannerlac_data_editor.py
+++ dannerlac_data_editor.py
@@ -20,10 +20,10 @@ if excel_file_path is None:
 df = pd.read_excel(excel_file_path)
 
 # Modify Quantity Available column to remove the sign
-df['Quantity Available'] = df['Quantity Available'].str.replace('+', '')
+df['Quantity Available'] = df['Quantity Available'].astype(str).str.replace('+', '')
 
 # Add a new column "New SKU"
-df['New SKU'] = df['Style Number'] + "-" + df['Color Code'] + "-" + df['Size'] + df['Alt Size']
+df['New SKU'] = df['Style Number'].astype(str) + "-" + df['Color Code'].astype(str) + "-" + df['Size'].astype(str) + df['Alt Size'].astype(str)
 
 # Save the DataFrame as a CSV file
 csv_file_path = os.path.splitext(excel_file_path)[0] + ".csv"
