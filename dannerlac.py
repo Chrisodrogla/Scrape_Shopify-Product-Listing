38c0db075e817672448bf101648f58644b08f3dd
Christian <c.algordo@yahoo.com>
2024-02-07 12:58:47 +0800

    Commit

--- dannerlac.py
+++ dannerlac.py
@@ -1,6 +1,7 @@
 import os
 import time
 from selenium import webdriver
+import shutil
 
 # Get secrets from environment variables
 username = os.environ['D_USERNAME_SECRET']
@@ -8,6 +9,13 @@ password = os.environ['D_PASSWORD_SECRET']
 
 # Define the repository directory
 repo_directory = os.getcwd()  # This gets the current working directory (your repository directory)
+
+# Define the downloads directory within the repository
+downloads_directory = os.path.join(repo_directory, 'Downloads')
+
+# Ensure the Downloads directory exists, create it if it doesn't
+if not os.path.exists(downloads_directory):
+    os.makedirs(downloads_directory)
 
 website = 'https://lfi.elasticsuite.com/#builder,order'
 
@@ -19,7 +27,7 @@ options.add_argument("--no-sandbox")
 options.add_argument("--disable-gpu")
 options.add_argument("--window-size=1920x1080")
 options.add_experimental_option("prefs", {
-    "download.default_directory": repo_directory,  # Set the download directory to the repository directory
+    "download.default_directory": downloads_directory,  # Set the download directory to the Downloads directory
     "download.prompt_for_download": False,
     "download.directory_upgrade": True,
     "safebrowsing.enabled": True
@@ -47,12 +55,13 @@ Allproducts=driver.find_element("xpath", """//*[@id="uniqName_14_3_all"]""").cli
 time.sleep(10)
 Export=driver.find_element("xpath", """//*[@id="uniqName_14_3"]/div[3]/span[2]/span""").click()
 
+time.sleep(100)
 
-time.sleep(30)
+# Move the downloaded file to the Downloads directory
+for filename in os.listdir(downloads_directory):
+    if filename.endswith('.xlsx'):  # Modify this condition based on the file type you're downloading
+        source_file = os.path.join(downloads_directory, filename)
+        destination_file = os.path.join(downloads_directory, 'Downloads', filename)
+        shutil.move(source_file, destination_file)
 
 driver.quit()
-
-
-
-
-
