3708be5a2c6c72514f60816c759cadc23f98ed27
Christian <c.algordo@yahoo.com>
2024-02-08 04:39:02 +0800

    Commit

--- dannerlac_data_editor.py
+++ dannerlac_data_editor.py
@@ -1,7 +1,10 @@
 import pandas as pd
+import os
+
+# Construct the file path relative to the script's location
+file_path = os.path.join(os.path.dirname(__file__), "Downloads", "Downloads", "Danner LaCrosse At Once.xlsx")
 
 # Read the Excel file
-file_path = "Downloads/Downloads/Danner LaCrosse At Once.xlsx"
 df = pd.read_excel(file_path)
 
 # Modify Quantity Available column to remove the '+' sign and convert to numeric
@@ -11,7 +14,7 @@ df['Quantity Available'] = df['Quantity Available'].str.replace('+', '').astype(
 df['New SKU'] = df['Style Number'] + '-' + df['Color Code'] + '-' + df['Size'] + df['Alt Size']
 
 # Save the modified DataFrame to a CSV file
-csv_file_path = "Downloads/Downloads/Danner LaCrosse At Once.csv"
+csv_file_path = os.path.join(os.path.dirname(__file__), "Downloads", "Downloads", "Danner LaCrosse At Once.csv")
 df.to_csv(csv_file_path, index=False)
 
 print("CSV file saved successfully.")
