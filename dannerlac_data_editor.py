81ea087ab28d60de65f72a069c5a010eed797038
Christian <c.algordo@yahoo.com>
2024-02-08 04:58:59 +0800

    Commit

--- dannerlac_data_editor.py
+++ dannerlac_data_editor.py
@@ -5,11 +5,8 @@ import os
 current_dir = os.getcwd()
 print("Current working directory:", current_dir)
 
-# Get the directory of the script
-script_dir = os.path.dirname(os.path.abspath(__file__))
-
-# Construct the file path relative to the script's location
-file_path = os.path.join(script_dir, "Downloads", "Downloads", "Danner LaCrosse At Once.xlsx")
+# Construct the file path relative to the current working directory
+file_path = os.path.join(current_dir, "Downloads", "Downloads", "Danner LaCrosse At Once.xlsx")
 
 # Read the Excel file
 df = pd.read_excel(file_path)
@@ -21,7 +18,7 @@ df['Quantity Available'] = df['Quantity Available'].str.replace('+', '').astype(
 df['New SKU'] = df['Style Number'] + '-' + df['Color Code'] + '-' + df['Size'] + df['Alt Size']
 
 # Save the modified DataFrame to a CSV file
-csv_file_path = os.path.join(script_dir, "Downloads", "Downloads", "Danner LaCrosse At Once.csv")
+csv_file_path = os.path.join(current_dir, "Downloads", "Downloads", "Danner LaCrosse At Once.csv")
 df.to_csv(csv_file_path, index=False)
 
 print("CSV file saved successfully.")
