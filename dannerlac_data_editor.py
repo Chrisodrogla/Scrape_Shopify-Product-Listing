d27d119b5223cd560aad1fc441181f20b9c46f5b
Christian <c.algordo@yahoo.com>
2024-02-08 05:24:39 +0800

    Commit

--- dannerlac_data_editor.py
+++ dannerlac_data_editor.py
@@ -1,7 +1,20 @@
+import os
 import pandas as pd
 
-# Define the path to the Excel file
-excel_file_path = "Downloads/Downloads/Danner LaCrosse At Once.xlsx"
+# Define the directory path
+directory = "Downloads/Downloads/"
+
+# Find the Excel file with the end name ".xlsx"
+excel_file_path = None
+for filename in os.listdir(directory):
+    if filename.endswith(".xlsx"):
+        excel_file_path = os.path.join(directory, filename)
+        break
+
+# If no Excel file is found, exit the script
+if excel_file_path is None:
+    print("No Excel file found in the directory.")
+    exit()
 
 # Read the Excel file
 df = pd.read_excel(excel_file_path)
@@ -13,7 +26,7 @@ df['Quantity Available'] = df['Quantity Available'].str.replace('+', '')
 df['New SKU'] = df['Style Number'] + "-" + df['Color Code'] + "-" + df['Size'] + df['Alt Size']
 
 # Save the DataFrame as a CSV file
-csv_file_path = "Downloads/Downloads/Danner LaCrosse At Once.csv"
+csv_file_path = os.path.splitext(excel_file_path)[0] + ".csv"
 df.to_csv(csv_file_path, index=False)
 
 print("CSV file saved successfully.")
