020829a9b42af66dced06c68d800273171084155
Christian Algordo <123637289+Chrisodrogla@users.noreply.github.com>
2024-03-06 02:48:23 +0800

    Update dannerlac_data_editor.py

--- dannerlac_data_editor.py
+++ dannerlac_data_editor.py
@@ -31,8 +31,8 @@ df['Alt Size'] = df['Alt Size'].fillna('')
 # Add a new column "New SKU"
 df['New SKU'] = df['Style Number'].astype(str) + "-" + df['Color Code'] + "-" + df['Size'].astype(str) + df['Alt Size'].astype(str)
 
-# Save the DataFrame as a CSV file
+# Save the DataFrame as a CSV file, treating Color Code column as text
 csv_file_path = os.path.splitext(excel_file_path)[0] + ".csv"
-df.to_csv(csv_file_path, index=False)
+df.to_csv(csv_file_path, index=False, dtype={'Color Code': str})
 
 print("CSV file saved successfully.")
