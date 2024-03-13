dda98df4e47396e75592d91529e679357efaf22e
Christian Algordo <123637289+Chrisodrogla@users.noreply.github.com>
2024-03-14 02:49:58 +0800

    Update twitter_scraper.py

--- Twitter_Scraper/twitter_scraper.py
+++ Twitter_Scraper/twitter_scraper.py
@@ -2,16 +2,13 @@ import os
 import time
 import datetime
 from selenium import webdriver
-import shutil
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import pandas as pd
-import os
-from datetime import datetime
 
 # Get secrets from environment variables
-username = os.environ['TWTR_USER_NAME']
-password = os.environ['TWTR_USER_PASS']
+username = os.environ.get('TWTR_USER_NAME')
+password = os.environ.get('TWTR_USER_PASS')
 email = 'christopherchan645@gmail.com'
 
 # Define the repository directory
@@ -20,12 +17,8 @@ repo_directory = os.getcwd()  # This gets the current working directory (your re
 # Define the downloads directory within the repository
 downloads_directory = os.path.join(repo_directory, 'Twitter_Scraper/Daily_Data')
 
-website = 'https://twitter.com/i/flow/login?newtwitter=true'
-
 # Define the path to the Chrome extension
 extension_path = 'Twitter_Scraper/JGEJDCDOEEABKLEPNKDBGLGCCJPDGPMF_1_8_2_0.crx'
-
-Login = "https://twitter.com/i/flow/login?newtwitter=true"
 
 # Set up Chrome WebDriver with custom download directory
 options = webdriver.ChromeOptions()
@@ -34,119 +27,113 @@ options.add_argument("--disable-dev-shm-usage")
 options.add_argument("--no-sandbox")
 options.add_argument("--disable-gpu")
 options.add_argument("--window-size=1920x1080")
+options.add_argument("--disable-extensions")  # Disable extensions
+options.add_argument("--disable-dev-tools")   # Disable developer tools
 options.add_experimental_option("prefs", {
-    "download.default_directory": downloads_directory,  # Set the download directory to the Downloads directory
+    "download.default_directory": downloads_directory,  
     "download.prompt_for_download": False,
     "download.directory_upgrade": True,
     "safebrowsing.enabled": True
 })
 
-# Define Chrome options
-chrome_options = webdriver.ChromeOptions()
-
-# Add the extension to Chrome options
-chrome_options.add_extension(extension_path)
-
-# Merge Chrome options
-options.add_argument(f"--load-extension={extension_path}")
-
-# Create WebDriver instance with merged Chrome options
+# Create WebDriver instance with Chrome options
 driver = webdriver.Chrome(options=options)
 
-driver.get(Login)
+# Define URLs
+login_url = "https://twitter.com/login"
+main_twitter_url = "https://twitter.com/gotbit_io/following"
 
-
-username_input = WebDriverWait(driver, 10).until(EC.presence_of_element_located(("xpath", """//*[@id="layers"]/div/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div/div/div/div[5]/label/div/div[2]/div/input""")))
-
-username_input.send_keys(username)
-
-next_button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable(("xpath", """//*[@id="layers"]/div/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div/div/div/div[6]/div""")))
-next_button.click()
-
-password_input = WebDriverWait(driver, 10).until(EC.presence_of_element_located(("xpath", """//*[@id="layers"]/div/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div[1]/div/div/div[3]/div/label/div/div[2]/div[1]/input""")))
-
-password_input.send_keys(password)
-
-login_button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable(("xpath", """//*[@id="layers"]/div/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div[2]/div/div[1]/div/div/div/div""")))
-login_button.click()
-
-time.sleep(3)
-
-try:
-    
-    email_input = WebDriverWait(driver, 10).until(EC.presence_of_element_located(("xpath", """//input[@autocomplete='email']""")))
-
-    email_input.send_keys(email)
-    time.sleep(3)
-
-    login_button2 = WebDriverWait(driver, 10).until(EC.element_to_be_clickable(("xpath", """//*[@id="layers"]/div/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div[2]/div/div/div/div/div""")))
-    login_button2.click() 
-except:
-    pass
-
-time.sleep(4)
-main_twitter = "https://twitter.com/gotbit_io/following"
-driver.get(main_twitter)
-time.sleep(10)
-
+# Function to scroll to the bottom of the page
 def scroll_to_bottom(driver):
-    # Get the height of the current page
     last_height = driver.execute_script("return document.body.scrollHeight")
 
     while True:
-        # Scroll down to the bottom
         driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-
-        # Wait for some time to load content
-        time.sleep(10)
-
-        # Calculate new height after scrolling
+        time.sleep(2)
         new_height = driver.execute_script("return document.body.scrollHeight")
 
-        # If the height doesn't change, then we've reached the bottom
         if new_height == last_height:
             break
 
         last_height = new_height
 
-# Call the function to scroll down
-scroll_to_bottom(driver)
+# Function to login to Twitter
+def login_to_twitter():
+    driver.get(login_url)
 
+    try:
+        # Wait for username input to load
+        username_input = WebDriverWait(driver, 10).until(EC.presence_of_element_located(("xpath", "//input[@name='session[username_or_email]']")))
+        username_input.send_keys(username)
 
-User_Name = driver.find_elements("xpath", "//div[@class='user-item-text']/span[1]")
-User_Mail = driver.find_elements("xpath", "//div[@class='user-item-text']/span[2]")
+        # Wait for next button to be clickable and click it
+        next_button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable(("xpath", "//div[@data-testid='LoginForm_Login_Button']")))
+        next_button.click()
 
-User_Links = driver.find_elements("xpath", "//a[@class='user-item-link']")
+        # Wait for password input to load
+        password_input = WebDriverWait(driver, 10).until(EC.presence_of_element_located(("xpath", "//input[@name='session[password]']")))
+        password_input.send_keys(password)
 
-# Concatenate the two lists
-Followers = zip(User_Name, User_Mail)
+        # Wait for login button to be clickable and click it
+        login_button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable(("xpath", "//div[@data-testid='LoginForm_Login_Button']")))
+        login_button.click()
 
-following_list = []
-acc_link = []
+        time.sleep(3)  # Adding a sleep for page to load
+    except Exception as e:
+        print("Exception occurred during login:", e)
 
+# Function to scrape following list
+def scrape_following():
+    driver.get(main_twitter_url)
+    time.sleep(5)  # Adding a sleep for page to load
 
-for user_name, user_mail in Followers:
-    formatted_user = f"{user_name.text} ({user_mail.text})"
-    following_list.append(formatted_user)
+    scroll_to_bottom(driver)
 
+    # Find user names, emails, and links
+    user_names = driver.find_elements_by_xpath("//div[@class='user-item-text']/span[1]")
+    user_mails = driver.find_elements_by_xpath("//div[@class='user-item-text']/span[2]")
+    user_links = driver.find_elements_by_xpath("//a[@class='user-item-link']")
 
-for link in User_Links:
-    userl = link.get_attribute('href')
-    acc_link.append(userl)
+    following_list = []
+    acc_link = []
 
-df = pd.DataFrame({'User': following_list, 'User_Link': acc_link})
+    for user_name, user_mail in zip(user_names, user_mails):
+        formatted_user = f"{user_name.text} ({user_mail.text})"
+        following_list.append(formatted_user)
 
-directory = os.path.join(repo_directory, 'Twitter_Scraper/Daily_Data')
+    for link in user_links:
+        user_link = link.get_attribute('href')
+        acc_link.append(user_link)
 
+    df = pd.DataFrame({'User': following_list, 'User_Link': acc_link})
 
-# Create the directory if it doesn't exist
-os.makedirs(directory, exist_ok=True)
+    return df
 
-# Generate filename based on current date and time
-current_time = datetime.now().strftime("%m-%d-%Y-%H-%M")
-filename = os.path.join(directory, f"{current_time}.json")
+# Function to save data to JSON file
+def save_to_json(df):
+    directory = os.path.join(repo_directory, 'Twitter_Scraper/Daily_Data')
 
-# Save the DataFrame to JSON file with indentation and proper formatting
-df.to_json(filename, orient='records', indent=4)
+    # Create the directory if it doesn't exist
+    os.makedirs(directory, exist_ok=True)
 
-driver.quit()
+    # Generate filename based on current date and time
+    current_time = datetime.datetime.now().strftime("%m-%d-%Y-%H-%M")
+    filename = os.path.join(directory, f"{current_time}.json")
+
+    # Save the DataFrame to JSON file with indentation and proper formatting
+    df.to_json(filename, orient='records', indent=4)
+    print(f"Data saved to {filename}")
+
+# Main function
+def main():
+    try:
+        login_to_twitter()
+        df = scrape_following()
+        save_to_json(df)
+    except Exception as e:
+        print("An error occurred:", e)
+    finally:
+        driver.quit()
+
+if __name__ == "__main__":
+    main()
