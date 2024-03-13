31d4fcd8a99fe1a3af08aeecd36c5901fe517c74
Christian Algordo <123637289+Chrisodrogla@users.noreply.github.com>
2024-03-14 05:02:08 +0800

    Update twitter_scraper.py

--- Twitter_Scraper/twitter_scraper.py
+++ Twitter_Scraper/twitter_scraper.py
@@ -31,7 +31,7 @@ Login = "https://twitter.com/i/flow/login?newtwitter=true"
 
 # Set up Chrome WebDriver with custom download directory
 options = webdriver.ChromeOptions()
-options.add_argument("--headless")
+# options.add_argument("--headless")
 options.add_argument("--disable-dev-shm-usage")
 options.add_argument("--no-sandbox")
 options.add_argument("--disable-gpu")
