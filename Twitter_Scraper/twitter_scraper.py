aa4fb74b48802fe9a491b94c4e0b13fadfe8ea05
Christian Algordo <123637289+Chrisodrogla@users.noreply.github.com>
2024-03-14 00:32:25 +0800

    Update twitter_scraper.py

--- Twitter_Scraper/twitter_scraper.py
+++ Twitter_Scraper/twitter_scraper.py
@@ -1,49 +1,49 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-import time
-import pandas as pd
 import os
-from datetime import datetime
+import time
+import datetime
+from selenium import webdriver
 import shutil
 
+# Get secrets from environment variables
 username = os.environ['TWTR_USER_NAME']
 password = os.environ['TWTR_USER_PASS']
 
+
+# Define the repository directory
 repo_directory = os.getcwd()  # This gets the current working directory (your repository directory)
 
 # Define the downloads directory within the repository
 downloads_directory = os.path.join(repo_directory, 'Twitter_Scraper/Daily_Data')
 
-time.sleep(5)
-# Path to the downloaded extension .crx file
-extension_path = 'Twitter_Scraper/JGEJDCDOEEABKLEPNKDBGLGCCJPDGPMF_1_8_2_0.crx'
-Login = "https://twitter.com/i/flow/login?newtwitter=true"
+# Ensure the Downloads directory exists, create it if it doesn't
+if not os.path.exists(downloads_directory):
+    os.makedirs(downloads_directory)
 
-# Set up Chrome WebDriver with custom download directory
+website = 'https://twitter.com/i/flow/login?newtwitter=true'
+
+# Define the path to the Chrome extension
+extension_path = 'https://twitter.com/i/flow/login?newtwitter=true'
+
+# Set up Chrome WebDriver with custom download directory and extension
 options = webdriver.ChromeOptions()
 options.add_argument("--headless")
 options.add_argument("--disable-dev-shm-usage")
 options.add_argument("--no-sandbox")
 options.add_argument("--disable-gpu")
 options.add_argument("--window-size=1920x1080")
+options.add_argument(f"--load-extension={extension_path}")  # Add the extension
+options.add_experimental_option("prefs", {
+    "download.default_directory": downloads_directory,  # Set the download directory to the Downloads directory
+    "download.prompt_for_download": False,
+    "download.directory_upgrade": True,
+    "safebrowsing.enabled": True
+})
 
-
-# Define Chrome options
-chrome_options = webdriver.ChromeOptions()
-
-# Add the extension to Chrome options
-chrome_options.add_extension(extension_path)
-
-# Merge Chrome options
-options.add_argument(f"--load-extension={extension_path}")
-
-# Create WebDriver instance with merged Chrome options
 driver = webdriver.Chrome(options=options)
-
-driver.get(Login)
+driver.get(website)
 time.sleep(5)
+
+
 username_input = WebDriverWait(driver, 10).until(EC.presence_of_element_located(("xpath", """//*[@id="layers"]/div/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div/div/div/div[5]/label/div/div[2]/div/input""")))
 
 username_input.send_keys(username)
@@ -58,10 +58,10 @@ password_input.send_keys(password)
 login_button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable(("xpath", """//*[@id="layers"]/div/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div[2]/div/div[1]/div/div/div/div""")))
 login_button.click()
 
-time.sleep(5)
+time.sleep(4)
 main_twitter = "https://twitter.com/gotbit_io/following"
 driver.get(main_twitter)
-time.sleep(5)
+time.sleep(10)
 
 def scroll_to_bottom(driver):
     # Get the height of the current page
@@ -72,7 +72,7 @@ def scroll_to_bottom(driver):
         driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
 
         # Wait for some time to load content
-        time.sleep(4)
+        time.sleep(10)
 
         # Calculate new height after scrolling
         new_height = driver.execute_script("return document.body.scrollHeight")
@@ -85,7 +85,8 @@ def scroll_to_bottom(driver):
 
 # Call the function to scroll down
 scroll_to_bottom(driver)
-time.sleep(5)
+
+
 User_Name = driver.find_elements("xpath", "//div[@class='user-item-text']/span[1]")
 User_Mail = driver.find_elements("xpath", "//div[@class='user-item-text']/span[2]")
 
@@ -96,7 +97,7 @@ Followers = zip(User_Name, User_Mail)
 
 following_list = []
 acc_link = []
-time.sleep(5)
+
 
 for user_name, user_mail in Followers:
     formatted_user = f"{user_name.text} ({user_mail.text})"
@@ -109,20 +110,17 @@ for link in User_Links:
 
 df = pd.DataFrame({'User': following_list, 'User_Link': acc_link})
 
-# Specify the directory path
 directory = os.path.join(repo_directory, 'Twitter_Scraper/Daily_Data')
-time.sleep(5)
+
+
 # Create the directory if it doesn't exist
 os.makedirs(directory, exist_ok=True)
 
 # Generate filename based on current date and time
 current_time = datetime.now().strftime("%m-%d-%Y-%H-%M")
 filename = os.path.join(directory, f"{current_time}.json")
-time.sleep(5)
+
 # Save the DataFrame to JSON file with indentation and proper formatting
 df.to_json(filename, orient='records', indent=4)
 
 driver.quit()
-
-
-
